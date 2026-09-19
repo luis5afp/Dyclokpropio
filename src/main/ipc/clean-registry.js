6b96f9eeed585a68021d19a3df91078a7d895f8e
luis5afp <luis5afp@gmail.com>
2026-09-18 22:16:08 -0500

    Migrate device info IPC to clean source

--- src/main/ipc/clean-registry.js
+++ src/main/ipc/clean-registry.js
@@ -1,14 +1,22 @@
 'use strict';
 
-const { registerProcessHandlers, CHANNELS: PROCESS_CHANNELS } = require('./handlers/process');
+const {
+  registerProcessHandlers,
+  CHANNELS: PROCESS_CHANNELS,
+} = require('./handlers/process');
+const {
+  registerDeviceInfoHandlers,
+} = require('../device/device-info');
 
 const MIGRATED_CHANNELS = Object.freeze([
   PROCESS_CHANNELS.GET_CURRENT_PLATFORM,
   PROCESS_CHANNELS.SET_PROCESS_ENV,
+  'get-device-info',
 ]);
 
 function registerMigratedHandlers(ipcMain) {
   registerProcessHandlers(ipcMain);
+  registerDeviceInfoHandlers(ipcMain);
 }
 
 module.exports = {
