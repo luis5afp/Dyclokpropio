637701d8359f7e3ad45bffc97562d8ee0243e8c0
luis5afp <luis5afp@gmail.com>
2026-09-18 22:14:20 -0500

    Reconstruct envSerialNums Vue SFC

---
+++ src/renderer/components/reconstructed/envSerialNums.vue
@@ -0,0 +1,19 @@
+<script setup>
+const props = defineProps(['serialNums', 'content'])
+</script>
+
+<template>
+  <div class="tw-flex tw-items-center">
+    <span class="serialNums-wrapper">
+      {{ props.content ? props.content : props.serialNums.join(' , ') }}
+    </span>
+  </div>
+</template>
+
+<style scoped>
+.serialNums-wrapper {
+  display: flex;
+  gap: 8px;
+  flex-wrap: nowrap;
+}
+</style>
